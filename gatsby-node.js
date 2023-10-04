e37293f937533dbc4cab6c1f49673c61a7a91c9e
Hirantha12 <hiranthaviraj@gmail.com>
2023-10-05 03:40:51 +0530

    feat: build_error

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,30 @@
 exports.createPages = async ({ actions: { createPage }, graphql }) => {
-  const data = await graphql(`
-    {
-      allDataJson {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-  `)
+  // const data = await graphql(`
+  //   {
+  //     allDataJson {
+  //       edges {
+  //         node {
+  //           slug
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
-  if (data.errors) {
-    console.log("Error retriving data", data.errors)
-    return
-  }
+  // if (data.errors) {
+  //   console.log("Error retriving data", data.errors)
+  //   return
+  // }
 
-  const dataTemplate = require.resolve("./src/template/template.js")
+  // const dataTemplate = require.resolve("./src/template/template.js")
 
-  data.data.allDataJson.edges.forEach(edge => {
-    createPage({
-      path: `/events/${edge.node.slug}/`,
-      component: dataTemplate,
-      context: {
-        slug: edge.node.slug,
-      },
-    })
-  })
+  // data.data.allDataJson.edges.forEach(edge => {
+  //   createPage({
+  //     path: `/events/${edge.node.slug}/`,
+  //     component: dataTemplate,
+  //     context: {
+  //       slug: edge.node.slug,
+  //     },
+  //   })
+  // })
 }
