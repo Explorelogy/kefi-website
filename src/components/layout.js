483a0edbc374afacb815c3a08403357b2e40d3a6
AmalChathuranga <amalcodochathuranga@gmail.com>
2023-11-09 12:32:24 +0530

    feat:gtag added

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useLocation } from "@reach/router"
 import Header from "./header"
 import Footer from "./footer"
+import { Helmet } from "react-helmet"
 
 const Layout = ({ children }) => {
 
@@ -9,6 +10,22 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=GTM-NT94DF9W"
+        ></script>
+        <script>
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'GTM-NT94DF9W');
+        `}
+        </script>
+      </Helmet>
+
         <Header />
         <main>{children}</main>
         {location.pathname == '/' ? (
